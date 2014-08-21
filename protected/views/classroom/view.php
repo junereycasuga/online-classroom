55b95392aff5c3775b0501e4010f8afaa3ec937b
Junerey Casuga <junereycasuga@Tagbonds-MacBook-Pro-2.local>
2014-08-21 11:40:11 +0800

    layout for classroom dashboard

--- protected/views/classroom/view.php
+++ protected/views/classroom/view.php
@@ -2,8 +2,25 @@
 	<h3><?php echo $model->classroom_name; ?></h3>
 </div>
 
-<ul class="nav nav-pills nav-stacked">
-	<li><a href="#">Assignments</a></li>
-	<li><a href="#">Quizzes</a></li>
-	<li><a href="#">Handouts</a></li>
+<ul class="nav nav-tabs" role="tablist">
+	<li class="active"><a href="#assignments" role="tab" data-toggle="tab">Assignments</a></li>
+	<li><a href="#quizzes" role="tab" data-toggle="tab">Quizzes</a></li>
+	<li><a href="#handouts" role="tab" data-toggle="tab">Handouts</a></li>
 </ul>
+
+<div class="tab-content">
+	<div class="tab-pane active" id="assignments">
+		<br>
+		<button class="btn btn-primary">New Assignment</button>
+	</div>
+
+	<div class="tab-pane" id="quizzes">
+		<br>
+		<button class="btn btn-primary">New Quiz</button>
+	</div>
+
+	<div class="tab-pane" id="handouts">
+		<br>
+		<button class="btn btn-primary">New Handout</button>
+	</div>
+</div>
