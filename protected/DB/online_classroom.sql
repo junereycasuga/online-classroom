ac3b6ea73096be09537fa987f512bc9f00c31b7c
Junerey Casuga <junerey.casuga@gmail.com>
2014-08-22 22:44:28 +0800

    db update

--- protected/DB/online_classroom.sql
+++ protected/DB/online_classroom.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 21, 2014 at 07:50 AM
--- Server version: 5.6.17
--- PHP Version: 5.4.24
+-- Generation Time: Aug 22, 2014 at 10:44 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -33,14 +33,37 @@ CREATE TABLE IF NOT EXISTS `assignments` (
   `assignment_body` longtext NOT NULL,
   `assignment_deadline` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `assignments`
 --
 
 INSERT INTO `assignments` (`id`, `classroom_id`, `assignment_title`, `assignment_body`, `assignment_deadline`) VALUES
-(1, 1, 'Assignment #1', 'Develop a web application that aggregates data about a certain area', '2014-11-30 06:23:22');
+(1, 1, 'Assignment #1', 'Develop a web application that aggregates data about a certain area', '2014-11-30 06:23:22'),
+(2, 0, '', '', '0000-00-00 00:00:00'),
+(3, 0, '', '', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment_replies`
+--
+
+CREATE TABLE IF NOT EXISTS `assignment_replies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `assignment_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `assignment_reply` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `assignment_replies`
+--
+
+INSERT INTO `assignment_replies` (`id`, `assignment_id`, `student_id`, `assignment_reply`) VALUES
+(1, 1, 2014001, 'Test reply');
 
 -- --------------------------------------------------------
 
@@ -63,6 +86,69 @@ CREATE TABLE IF NOT EXISTS `classroom` (
 
 INSERT INTO `classroom` (`id`, `classroom_name`, `classroom_subject`, `classroom_teacher`) VALUES
 (1, 'COM1 - Mang Jose', 1, 123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `handouts`
+--
+
+CREATE TABLE IF NOT EXISTS `handouts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `classroom_id` int(11) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `handouts`
+--
+
+INSERT INTO `handouts` (`id`, `classroom_id`, `file`) VALUES
+(1, 1, 'online_classroom.sql');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizzes`
+--
+
+CREATE TABLE IF NOT EXISTS `quizzes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `quiz_title` varchar(255) NOT NULL,
+  `quiz_body` longtext NOT NULL,
+  `quiz_deadline` datetime NOT NULL,
+  `classroom_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+INSERT INTO `quizzes` (`id`, `quiz_title`, `quiz_body`, `quiz_deadline`, `classroom_id`) VALUES
+(1, 'Quiz #1', 'Create an app', '2014-08-23 05:27:24', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_replies`
+--
+
+CREATE TABLE IF NOT EXISTS `quiz_replies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `quiz_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `quiz_reply` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `quiz_replies`
+--
+
+INSERT INTO `quiz_replies` (`id`, `quiz_id`, `student_id`, `quiz_reply`) VALUES
+(1, 1, 2014001, 'Okay');
 
 -- --------------------------------------------------------
 
@@ -104,8 +190,7 @@ CREATE TABLE IF NOT EXISTS `student_classroom` (
 --
 
 INSERT INTO `student_classroom` (`id`, `student_id`, `classroom_id`) VALUES
-(1, 2014001, 1),
-(2, 2014001, 1);
+(1, 2014001, 1);
 
 -- --------------------------------------------------------
 
@@ -162,14 +247,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_type`, `id`) VALUES
-(1, 'mangjose', '0655a949c0826968e2daa324026f7b2dcccd94ac882ba5a4d5cec0fbc1fc0ff545a1a3b77560b7ead1704ca9f41b73a91c7d238f946f723e66fa96a17f1c3558', 1, 123);
+(1, 'mangjose', '0655a949c0826968e2daa324026f7b2dcccd94ac882ba5a4d5cec0fbc1fc0ff545a1a3b77560b7ead1704ca9f41b73a91c7d238f946f723e66fa96a17f1c3558', 1, 123),
+(2, 'totoybrown', '0655a949c0826968e2daa324026f7b2dcccd94ac882ba5a4d5cec0fbc1fc0ff545a1a3b77560b7ead1704ca9f41b73a91c7d238f946f723e66fa96a17f1c3558', 2, 2014001);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
